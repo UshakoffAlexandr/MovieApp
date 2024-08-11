42fde778d132996690d1d5ba65e47280df9d716d
Alex <alex@Ushakov.local>
2024-08-11 12:52:39 +0500

    apps1

--- src/components/contexts/GenresContext.js
+++ src/components/contexts/GenresContext.js
@@ -5,19 +5,19 @@ export const GenresContext = createContext()
 export const GenresProvider = ({ children }) => {
   const [genres, setGenres] = useState([])
 
-  const getGenre = async () => {
-    const queryURL = `${this.baseURL}/genre/movie/list?language=en`
-    const res = await fetch(queryURL, this.options)
-    if (!res.ok) {
-      throw new Error(`Could not fetch ${queryURL} received ${res.status}`)
-    }
-    const allGenres = await res.json()
-    return allGenres
-  }
+  // const getGenre = async () => {
+  //   const queryURL = `${this.baseURL}/genre/movie/list?language=en`
+  //   const res = await fetch(queryURL, this.options)
+  //   if (!res.ok) {
+  //     throw new Error(`Could not fetch ${queryURL} received ${res.status}`)
+  //   }
+  //   const allGenres = await res.json()
+  //   return allGenres
+  // }
 
-  const saveGenres = (res = []) => {
-    setGenres(res)
-  }
+  // const saveGenres = (res = []) => {
+  //   setGenres(res)
+  // }
 
   useEffect(() => {
     const fetchGenres = async () => {
